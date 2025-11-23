dcaca3deecd8e7f71636735698f677157b670296
Ishini <misnfernando1@gmail.com>
2025-11-23 12:56:59 +0530

    Routes set to ListEmployees

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import Footer from './components/Footer';
+import Header from './components/Header';
 import ListEmployee from './components/ListEmployee'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return(
     <div>
-      <ListEmployee/>
+      <BrowserRouter>
+        <Header/>
+          <Routes>
+            {/* http://localhost:5173 */}
+            <Route path='/' element={<ListEmployee/>}></Route>
+
+            {/* http://localhost:5173/employees */}
+            <Route path='/employees' element={<ListEmployee/>}></Route>
+          </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
   
